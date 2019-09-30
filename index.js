8a222afef0f5316021514b7a48345c07e733f7ed
zachary-peasley <zpman95@hotmail.com>
2019-09-30 18:08:13 -0500

    Need to test it out in insomnia

--- index.js
+++ index.js
@@ -23,16 +23,20 @@ server.get("/", (req, res) => {
 })
 
 server.post('/api/users', (req, res) => {
-    const user = req.body;
-    DataBase.insert(user)
+    const { name, bio } = req.body
+    if ( !name|| !bio) {
+        res.status(400)
+        .json({ error: 'Please provide name and bio for the user.'})
+    } else {
+    DataBase.insert(req.body)
     .then(obj => DataBase.findById(obj.id))
     .then(user => {
         res.status(201).json(user)
     })
-    .catch(error => {
-        console.log(error);
+    .catch(()=> {
         res.status(500).json({error: "There was an error while saving the user to the database"})
     })
+}
 })
 
 
@@ -62,20 +66,49 @@ server.post('/api/users', (req, res) => {
 
 
 
+server.delete('/api/users/:id', (req,res) => {
+    const id = req.params.id
+    DataBase.remove(id)
+    .then(count => {
+        if(count && count > 0) {
+            res.status(200).json({message: 'the user was deleted'})
+} else {
+    res
+    .status(404)
+    .json({message: "The user with the specified ID does not exist."})
+}
+})
+.catch(() => {
+    res.status(500).json({error: 'The user could not be removed'});
+})
+})
 
 
 
 
+server.put('/api/users/:id', (req, res) => {
+    const { name, bio } = req.body;
+if (!name || !bio ) {
+    res
+    .status(400)
+    .json({message: 'Please provide name and bio for user'})
+    } else {
+        DataBase.update(req.params.id, req.body)
+        .then(user => {
+            if(user) {
+                res.status(200).json(user)
+            } else {
+                res
+                .status(404)
+                .json({message: 'The user with the specified ID does not exist'});
+        }
+    })
 
-
-
-
-
-
-
-
-
-
+    .catch(() => {
+    res.status(500).json({message: 'The user information could not be modified.',});
+        });
+     }
+    });
  
 
 
